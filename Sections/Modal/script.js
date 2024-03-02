ea5aa56b2cbcb3d096f33adc1594eea96f5deeca
Hady Korshem <itshadyy@gmail.com>
2024-03-02 09:06:00 +0200

    Add files via upload

---
+++ Sections/Modal/script.js
@@ -0,0 +1,13 @@
+function openModal(){
+    document.getElementById("modal").style.visibility = "visible";
+    document.getElementById("modal").style.opacity = "1";
+}
+
+function closeModal(){
+    document.getElementById("modal").style.visibility = "hidden";
+    document.getElementById("modal").style.opacity = "0";
+}
+
+function message(){
+    alert("You've been subscribed successfully!!!!")
+}
