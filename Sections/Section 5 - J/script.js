2a811de2806b214e1760f3a386e8a1fe8cbb5ebe
Hady Korshem <itshadyy@gmail.com>
2024-04-22 20:37:41 +0200

    Add files via upload

---
+++ Sections/Section 5 - J/script.js
@@ -0,0 +1,11 @@
+function openNav(){
+    document.getElementById("sideNav").style.width = "220px";
+    document.getElementById("main").style.paddingLeft = "220px";
+
+}
+
+function closeNav(){
+    document.getElementById("sideNav").style.width = "0px";
+    document.getElementById("main").style.paddingLeft = "0px";
+
+}
