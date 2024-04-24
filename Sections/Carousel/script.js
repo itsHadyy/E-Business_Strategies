d5b053386e9eea3692d3c635bf20d6d77c3c7ccc
Hady Korshem <itshadyy@gmail.com>
2024-04-24 19:56:41 +0200

    Add files via upload

---
+++ Sections/Carousel/script.js
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\paperw11900\paperh16840\margl1440\margr1440\margt1440\margb1440\vieww18540\viewh17280\viewscale100\lin0\rin0
+{\papercolor16777215}
+{\fonttbl\f0\fnil\fcharset0 Helvetica;}
+{\colortbl\red0\green0\blue0;\red255\green255\blue255;}
+\pard
+{\*\pageHeader Helvetica::12px::right::black::0.5::NO::Header Author: Zoe Appleseed [date] - Page [pagenumber] of [totalpages]}
+{\*\pageFooter Helvetica::12px::right::black::0.5::NO::Footer Author: Zoe Appleseed [title] - Page [pagenumber] of [totalpages]}
+{\*\background {\shp{\*\shpinst\shpleft0\shptop0\shpright0\shpbottom0\shpfhdr0\shpbxmargin\shpbymargin\shpwr0\shpwrk0\shpfblwtxt1\shpz0\shplid1025{\sp{\sn shapeType}{\sv 1}}{\sp{\sn fFlipH}{\sv 0}}{\sp{\sn fFlipV}{\sv 0}}{\sp{\sn fillColor}{\sv 16777215}}{\sp{\sn fFilled}{\sv 1}}{\sp{\sn lineWidth}{\sv 0}}{\sp{\sn fLine}{\sv 0}}{\sp{\sn bWMode}{\sv 9}}{\sp{\sn fBackground}{\sv 1}}}}}
+\fi357
+\f0\fs24 \cf0 const slidesContainer = document.getElementById("slides-container");const slide = document.querySelector(".slide");const prevButton = document.getElementById("slide-arrow-prev");const nextButton = document.getElementById("slide-arrow-next");nextButton.addEventListener("click", () =>   const slideWidth = slide.clientWidth;  slidesContainer.scrollLeft += slideWidth;);prevButton.addEventListener("click", () =>   const slideWidth = slide.clientWidth;  slidesContainer.scrollLeft -= slideWidth;);\
+}
